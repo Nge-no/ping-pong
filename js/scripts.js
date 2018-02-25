fd9e77b59c45cd13cac8adc582513436a6cae068
Brian Ngeno <ngenobrian7@gmail.com>
2018-02-25 19:57:37 +0300

    add function to read user input in preparation for back-end manipulation

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    value = parseInt($("#value-x").val());
+    var result = display(value);
+    $("#output").text(result);
+  });
+});
